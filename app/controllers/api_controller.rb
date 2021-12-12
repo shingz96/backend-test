fc01cfabafd43a47aa5ada1c39132838cf878b80
kahshingchee <kahshing.chee@myfave.com>
2021-12-12 14:20:29 +0800

    list top posts api

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,44 @@
+class ApiController < ApplicationController
+  require 'net/http'
+
+  def list_posts
+    render json: top_posts
+  end
+
+  private
+
+  def comments
+    @_comments ||= Array.wrap(http_get("https://jsonplaceholder.typicode.com/comments"))
+  end
+
+  def comments_count_by_post_id(post_id)
+    @_comments_count_by_post_id ||= comments.each_with_object(Hash.new) { |comment, hash| hash[comment['postId']] = hash[comment['postId']].to_i + 1 }
+    @_comments_count_by_post_id.dig(post_id)
+  end
+
+  def top_posts
+    posts = []
+    records = Array.wrap(http_get('https://jsonplaceholder.typicode.com/posts'))
+    records.each do |post|
+      posts.push(
+        {
+          post_id: post['id'],
+          post_title: post['title'],
+          post_body: post['body'],
+          total_number_of_comments: comments_count_by_post_id(post['id']).to_i
+        }
+      )
+    end
+
+    posts.sort_by{ |post| [post[:total_number_of_comments] * -1, post[:post_id]] }
+  end
+
+  def http_get(url)
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+    JSON.parse(res.body)
+  rescue StandardError => e
+    puts e.message
+    nil
+  end
+end
